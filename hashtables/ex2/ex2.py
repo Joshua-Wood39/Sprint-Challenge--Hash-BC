d9c9435feacf88e75f9014f756ae1c643832278b
Joshua Wood <JerichoWall@apu.edu>
2019-11-02 21:18:31 -0700

    Finished MVP - still looking for a coin

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 class Ticket:
@@ -16,8 +14,14 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    for item in range(length):
+        flight_source = tickets[item].source
+        flight_destination = tickets[item].destination
+        hash_table_insert(hashtable, flight_source, flight_destination)
+
+    # looking for capital NONE
+    route[0] = hash_table_retrieve(hashtable, 'NONE')
+    for item in range(1, length):
+        route[item] = hash_table_retrieve(hashtable, route[item - 1])
 
     return route
